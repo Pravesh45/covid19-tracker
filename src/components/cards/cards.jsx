0f1e14a4bd531949e49be6a23ed72fd5364457f6
Pravesh45 <sid.k.45.27@gmail.com>
2020-04-22 19:55:30 +0530

    Added Last Updated Time

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -8,6 +8,12 @@ const Cards = ({ data: { confirmed, deaths, recovered, lastUpdate } }) => {
     if (!confirmed) {
         return "Loading......"
     }
+    const changeFormat = (item) => {
+        let amOrPm = item.getHours() >= 12 ? "pm" : "am"
+        let h = (item.getHours() % 12) || 12
+        let m = item.getMinutes()
+        return `${h}:${(item.getMinutes() / 10) > 1 ? "" : "0"}${m} ${amOrPm}`
+    }
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
@@ -16,8 +22,10 @@ const Cards = ({ data: { confirmed, deaths, recovered, lastUpdate } }) => {
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
                         <Typography variant="h5">
                             <CountUp start={0} end={confirmed.value} duration={2} separator="," />
-                        </Typography>
+                        </Typography><br />
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography color="textSecondary">{changeFormat(new Date(lastUpdate))}</Typography>
+                        <br />
                         <Typography variant="body2">Number of active cases of COVID-19</Typography>
                     </CardContent>
                 </Grid>
@@ -27,7 +35,10 @@ const Cards = ({ data: { confirmed, deaths, recovered, lastUpdate } }) => {
                         <Typography variant="h5">
                             <CountUp start={0} end={recovered.value} duration={2} separator="," />
                         </Typography>
+                        <br />
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography color="textSecondary">{changeFormat(new Date(lastUpdate))}</Typography>
+                        <br />
                         <Typography variant="body2">Number of cases recovered from COVID-19</Typography>
                     </CardContent>
                 </Grid>
@@ -36,8 +47,10 @@ const Cards = ({ data: { confirmed, deaths, recovered, lastUpdate } }) => {
                         <Typography color="textSecondary" gutterBottom>Death</Typography>
                         <Typography variant="h5">
                             <CountUp start={0} end={deaths.value} duration={2} separator="," />
-                        </Typography>
+                        </Typography><br />
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
+                        <Typography color="textSecondary">{changeFormat(new Date(lastUpdate))}</Typography>
+                        <br />
                         <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
                     </CardContent>
                 </Grid>
